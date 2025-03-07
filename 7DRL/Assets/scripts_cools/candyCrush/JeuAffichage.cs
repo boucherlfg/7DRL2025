a008ba6ab86de601ea4f5e84e736f9e0f3a053f4
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2025-03-06 22:28:14 -0500

    epik candy crush fonctionnel

--- 7DRL/Assets/scripts_cools/candyCrush/JeuAffichage.cs
+++ 7DRL/Assets/scripts_cools/candyCrush/JeuAffichage.cs
@@ -3,114 +3,136 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.VersionControl;
 using UnityEngine;
+using System.Collections.Generic;
+
+
 public class JeuAffichage : MonoBehaviour
 {
-   
-
-    public List<Sprite> sprites = new();
-    public List<TuileType> types = new();
-    public GameObject tilePrefab;
+    public static JeuAffichage Instance { get; private set; }
+    public GameObject[,] grid;
     public int dimension;
     public float distance;
-    public GameObject[,] grid;
+    public GameObject tilePrefab;
+    public List<TuileType> types;
+    private Vector3 positionOffset;
 
-    public bool jeuEnCours = false;
-
-
-    public JeuAffichage script;
-
-    public void Afficher(int dimension)
-    {
-        if (sprites.Count != 0 && tilePrefab != null)
-        {
-                grid = new GameObject[dimension, dimension];
-
-                Vector3 positionOffset = /**new Vector3(0, 0, 0)*/transform.position - new Vector3(dimension * distance / 2.0f - distance / 2, dimension * distance / 2.0f - distance / 2, 0);
-
-                for (int row = 0; row < dimension; row++)
-                {
-                    for (int column = 0; column < dimension; column++)
-                    {
-
-
-                        GameObject newTile = Instantiate(tilePrefab); 
-
-                        Tuiles tile = newTile.AddComponent<Tuiles>(); 
-                                                                      //a refaire pour etre proportionnel a inventaire;
-                        tile.type = types[Random.Range(0, types.Count)]; //good
-
-                        SpriteRenderer renderer = newTile.GetComponent<SpriteRenderer>(); 
-                        renderer.sprite = tile.type.sprite; 
-
-                        newTile.transform.parent = transform;
-                        newTile.transform.position = new Vector3(column * distance, row * distance, 0) + positionOffset;
-
-
-                        grid[column, row] = newTile;
-                        jeuEnCours = true;
-
-                    }
-            }
-        }
-        else
-        {
-            Debug.Log("JeuAffichage : y manque de quoi dans les attributs");
-        }
-    }
-
-
-    public void recommencer()
-    {
-        for (int row = 0; row < dimension; row++)
-        {
-            for (int column = 0; column < dimension; column++)
-            {
-                GameObject tuile = grid[column, row];
-                Tuiles tuileScript = tuile.GetComponent<Tuiles>();
-
-                tuileScript.type = types[Random.Range(0, types.Count)]; //good
-
-                SpriteRenderer renderer = tuile.GetComponent<SpriteRenderer>();
-                renderer.sprite = tuileScript.type.sprite;
-                renderer.color = Color.white;
-            }
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-    public void BougeTiles(int xInitial,int yInitial, int xDestination, int yDestination)
-    {
-        GameObject tile1 = grid[xInitial, yInitial];
-        SpriteRenderer renderer1 = tile1.GetComponent<SpriteRenderer>();
-
-        GameObject tile2 = grid[xDestination, yDestination];
-        SpriteRenderer renderer2 = tile2.GetComponent<SpriteRenderer>();
-
-
-
-        Sprite temp = renderer1.sprite;
-        renderer1.sprite = renderer2.sprite;
-        renderer2.sprite = temp;
-        
-    }
-
-
-    public static JeuAffichage Instance { get; private set; }
-    void Awake() { Instance = this; }
-
+    private void Awake() { Instance = this; }
 
     private void Start()
     {
-        Afficher(dimension);
-
+        positionOffset = transform.position - new Vector3(dimension * distance / 2.0f - distance / 2, dimension * distance / 2.0f - distance / 2, 0);
+        GenererGrille();
     }
-    
+
+    public void GenererGrille()
+    {
+        grid = new GameObject[dimension, dimension];
+        for (int x = 0; x < dimension; x++)
+        {
+            for (int y = 0; y < dimension; y++)
+            {
+                CreerTuile(x, y);
+            }
+        }
+    }
+
+    public void CreerTuile(int x, int y)
+    {
+        GameObject newTile = Instantiate(tilePrefab);
+        Tuiles tile = newTile.AddComponent<Tuiles>();
+        tile.type = types[Random.Range(0, types.Count)];
+
+        SpriteRenderer renderer = newTile.GetComponent<SpriteRenderer>();
+        renderer.sprite = tile.type.sprite;
+
+        newTile.transform.parent = transform;
+        newTile.transform.position = new Vector3(x * distance, y * distance, 0) + positionOffset;
+
+        grid[x, y] = newTile;
+    }
+
+    public List<Tuiles> ObtenirVoisins(Tuiles tuile)
+    {
+        List<Tuiles> voisins = new List<Tuiles>();
+        Vector2Int pos = ObtenirPositionTuile(tuile);
+        Vector2Int[] directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+
+        foreach (Vector2Int dir in directions)
+        {
+            int x = pos.x + dir.x;
+            int y = pos.y + dir.y;
+            if (x >= 0 && x < dimension && y >= 0 && y < dimension && grid[x, y] != null)
+            {
+                voisins.Add(grid[x, y].GetComponent<Tuiles>());
+            }
+        }
+        return voisins;
+    }
+
+    public Vector2Int ObtenirPositionTuile(Tuiles tuile)
+    {
+        for (int x = 0; x < dimension; x++)
+        {
+            for (int y = 0; y < dimension; y++)
+            {
+                if (grid[x, y] == tuile.gameObject)
+                    return new Vector2Int(x, y);
+            }
+        }
+        return Vector2Int.one * -1;
+    }
+
+    public void AppliquerGravite()
+    {
+        for (int x = 0; x < dimension; x++)
+        {
+            for (int y = 1; y < dimension; y++)
+            {
+                if (grid[x, y] != null && grid[x, y - 1] == null)
+                {
+                    for (int k = y; k > 0 && grid[x, k - 1] == null; k--)
+                    {
+                        grid[x, k - 1] = grid[x, k];
+                        grid[x, k] = null;
+                        grid[x, k - 1].transform.position = new Vector3(x * distance, (k - 1) * distance, 0) + positionOffset;
+                    }
+                }
+            }
+        }
+    }
+
+    public void DecalerColonnes()
+    {
+        for (int x = 0; x < dimension - 1; x++)
+        {
+            if (grid[x, 0] == null)
+            {
+                for (int k = x; k < dimension - 1; k++)
+                {
+                    for (int y = 0; y < dimension; y++)
+                    {
+                        grid[k, y] = grid[k + 1, y];
+                        if (grid[k, y] != null)
+                            grid[k, y].transform.position = new Vector3(k * distance, y * distance, 0) + positionOffset;
+                    }
+                }
+                for (int y = 0; y < dimension; y++)
+                    grid[dimension - 1, y] = null;
+            }
+        }
+    }
+
+    public void RemplirGrille()
+    {
+        for (int x = 0; x < dimension; x++)
+        {
+            for (int y = 0; y < dimension; y++)
+            {
+                if (grid[x, y] == null)
+                {
+                    CreerTuile(x, y);
+                }
+            }
+        }
+    }
 }
--- 7DRL/Assets/scripts_cools/candyCrush/Tuiles.cs
+++ 7DRL/Assets/scripts_cools/candyCrush/Tuiles.cs
@@ -1,203 +1,56 @@
-using JetBrains.Annotations;
 using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.UIElements;
 
 public class Tuiles : MonoBehaviour
 {
-    
-
-    private static Tuiles tuile; 
-    private SpriteRenderer Renderer;
-    
     public TuileType type;
-
-    public bool trouver = false;
-
-    public class TuilePosition
-    {
-        private GameObject tuile;
-        private int x;
-        private int y;
-        public TuilePosition(GameObject tuile, int x, int y)
-        {
-            this.tuile = tuile; 
-            this.x = x;
-            this.y = y;
-        }
-        public GameObject getTuile()
-        {
-            return tuile;
-        }
-        public int getX()
-        {
-            return x;
-        }
-        public int getY()
-        {
-            return y;
-        }
-    }
-
-    private void Start()
-    {
-        Renderer = GetComponent<SpriteRenderer>();
-    }
-
-    public void Click()
-    {
-        Renderer.color = Color.green;
-    }
-    public void Lache() 
-    {
-        Renderer.color = Color.white;
-    }
-
-
-
-    /**
-     * 
-     * Permet de trouver les tuiles similaire 
-     */
-    public void TrouverTuiles(int xInit, int yInit, List<TuilePosition> list)
-    {
-
-        int xDest = xInit-1;
-        int yDest = yInit-1;
-        bool saut = false;
-       for(int i = yDest; i < yDest + 3; i++)
-        {
-            for(int j = xDest; j < xDest + 3; j++)
-            {
-                if (saut == true)
-                {     
-                    saut = false;
-                    if (j >= 0 && j < JeuAffichage.Instance.dimension && i >= 0 && i < JeuAffichage.Instance.dimension)
-                    {
-                        if (JeuAffichage.Instance.grid[j, i].GetComponent<Tuiles>().type.ToString() == type.ToString() && JeuAffichage.Instance.grid[j, i].GetComponent<Tuiles>().trouver != true)
-                        {
-                            GameObject itemSimilaire = JeuAffichage.Instance.grid[j, i];
-                            TuilePosition nouvelleTuile = new(JeuAffichage.Instance.grid[j, i],i,j);
-                          
-                            trouver = true;
-                            list.Add(nouvelleTuile);
-
-
-                            nouvelleTuile.getTuile().GetComponent<SpriteRenderer>().color = Color.red;
-                        } 
-                    }
-                }
-                else
-                {
-                    saut = true;
-                }
-            }             
-        }
-    }
-    int bug = 0;
-    public void VerifieMatch(int xPos, int yPos)
-    {
-        List<GameObject> tuileARetirer = new List<GameObject>();
-        List<TuilePosition> tuileTrouver = new List<TuilePosition>();
-
-        TrouverTuiles(xPos, yPos, tuileTrouver);
-        if(tuileTrouver.Count != 0)
-        {
-            int loopProof = 0;
-            bool fin = false;
-            while( fin == false && loopProof < 20)
-            {
-                List<TuilePosition> tuileTemporaire = new();
-
-                for(int i = 0; i < tuileTrouver.Count; i++)
-                {
-
-                    TrouverTuiles(tuileTrouver[i].getX(), tuileTrouver[i].getY(), tuileTemporaire);
-                    tuileARetirer.Add(tuileTrouver[i].getTuile());
-                }
-                tuileTrouver.Clear();
-                for(int i = 0; i < tuileTemporaire.Count; i++)
-                {
-                    tuileTrouver.Add(tuileTemporaire[i]);
-                }
-                if (tuileTrouver.Count == 0)
-                {
-                    fin = true;
-                }
-                loopProof++;
-            }
-            for(int i =0; i < tuileARetirer.Count; i++)
-            {
-                tuileARetirer[i].GetComponent<SpriteRenderer>().color = Color.red;
-            }
-        }
-        else
-        {
-            Debug.Log("rien trouver");
-        }
-    }
 
     private void OnMouseDown()
     {
-
-        int xInitial = 0;
-        int yInitial = 0;
-        int xDestination = 0;
-        int yDestination = 0;
-
-        bool tuileTrouver = false;
-        int limite = 0;
-
-        List<TuilePosition> whatever = new();
-  
-            for(int i = 0; i < JeuAffichage.Instance.dimension; i++)
-            {
-                for(int j = 0; j < JeuAffichage.Instance.dimension; j++)
-                {
-                    if (JeuAffichage.Instance.grid[i, j] == transform.gameObject)
-                    {
-                        xInitial = i;
-                        yInitial = j;
-                        xDestination = i;
-                        yDestination = j - 1;
-                    }
-                }
-            }
-
-
-        //VerifieMatch(xInitial, yInitial);
-        TrouverTuiles(xInitial, yInitial, whatever);
-
-            while (tuileTrouver == false && limite < 5)
-            {
-
-                if (xDestination>=0 && xDestination < JeuAffichage.Instance.dimension && yDestination >= 0 && yDestination < JeuAffichage.Instance.dimension)
-                {
-                    if (JeuAffichage.Instance.grid[xDestination, yDestination] == gameObject)
-                    {
-                        tuileTrouver = true;
-                    }    
-                }
-                if(tuileTrouver == false)
-                {
-                    xDestination++;
-
-                    if(yDestination == yInitial-1 && xDestination == xInitial+1)
-                    {
-                        xDestination = xInitial-1;
-                        yDestination++;
-                    }
-                    if(yDestination == yInitial && xDestination == xInitial+2)
-                    {
-                        xDestination = xInitial;
-                        yDestination++;
-                    }
-                }
-                limite++;
-            }
+        List<Tuiles> tuilesASupprimer = TrouverTuilesAdjacentes();
+        if (tuilesASupprimer.Count > 0)
+        {
+            SupprimerTuiles(tuilesASupprimer);
+            JeuAffichage.Instance.AppliquerGravite();
+            JeuAffichage.Instance.DecalerColonnes();
+            //JeuAffichage.Instance.RemplirGrille();
         }
-
     }
 
+    private List<Tuiles> TrouverTuilesAdjacentes()
+    {
+        List<Tuiles> tuilesTrouvees = new List<Tuiles>();
+        Queue<Tuiles> queue = new Queue<Tuiles>();
+        HashSet<Tuiles> visited = new HashSet<Tuiles>();
+
+        queue.Enqueue(this);
+        visited.Add(this);
+
+        while (queue.Count > 0)
+        {
+            Tuiles current = queue.Dequeue();
+            tuilesTrouvees.Add(current);
+
+            foreach (Tuiles voisin in JeuAffichage.Instance.ObtenirVoisins(current))
+            {
+                if (!visited.Contains(voisin) && voisin.type == this.type)
+                {
+                    queue.Enqueue(voisin);
+                    visited.Add(voisin);
+                }
+            }
+        }
+        return tuilesTrouvees;
+    }
+
+    private void SupprimerTuiles(List<Tuiles> tuilesASupprimer)
+    {
+        foreach (Tuiles tuile in tuilesASupprimer)
+        {
+            Vector2Int pos = JeuAffichage.Instance.ObtenirPositionTuile(tuile);
+            JeuAffichage.Instance.grid[pos.x, pos.y] = null;
+            Destroy(tuile.gameObject);
+        }
+        Debug.Log($"{tuilesASupprimer.Count} tuiles de type {type} supprimées.");
+    }
+}
