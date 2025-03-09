6efb9a9481f8a62435e0ce7e5928cb0be87f6b20
Jasmine-Lapierre <2188895@cmontmorency.qc.ca>
2025-03-08 19:15:32 -0500

    buildfonctionnel

--- 7DRL/Assets/scripts_cools/candyCrush/JeuAffichage.cs
+++ 7DRL/Assets/scripts_cools/candyCrush/JeuAffichage.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
